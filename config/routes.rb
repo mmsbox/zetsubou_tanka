5e5814f2d1102e8948051e634c97f09f741d1725
mmsbox <mo100perfume@gmail.com>
2026-08-15 12:46:29 +0900

    fix: ogpルーティングにpngフォーマットのデフォルト設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
 
   resources :posts, only: [ :index, :show, :new, :create ] do
     member do
-      get :ogp
-      post :like
+    get :ogp, defaults: { format: :png, :jpg }
+    post :like
     end
   end
 end
