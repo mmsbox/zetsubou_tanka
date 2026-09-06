f4c4b56f83fa4e4d0b69b276bd2371b19b754f8c
mmsbox <mo100perfume@gmail.com>
2026-09-06 16:58:29 +0900

    fix: PostsControllerTest のテスト重複を解消

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,12 +1,17 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
     get posts_url
     assert_response :success
   end
 
   test "should get new" do
+    user = users(:one)
+    sign_in user
+
     get new_post_url
     assert_response :success
   end
